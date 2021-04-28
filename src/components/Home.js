254b2215875b9fce3a4ce46eb51e094c29510115
dehorus <ruizm.aj@gmail.com>
2021-04-28 15:54:26 -0600

    SVG and HOME header added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import logo from '../logo.svg'
+import "./Home.scss";
+import MoonIllustration from './MoonIllustration';
 
 export default  function Home() {
     return (
-        <header>
-            <h1>HOME PAGE!</h1>
-        </header>
+        <main className="home">
+            <div className="container-mobile">
+                <div className="text-container">
+                 <img className="mylogo" src={logo} alt="myLogo"/>
+                 <h3>Hi! my name is Angel and i'm a Front-end developer. </h3>
+                </div>
+            </div>
+            <MoonIllustration></MoonIllustration>
+        </main>
     ) 
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom';
-import "./NavBar.scss"
+import "./NavBar.scss";
+import LinkeDin from '../draws/LinkeDin.svg';
+import CodePen from '../draws/CodePen.svg';
+import GitHub from '../draws/Github.svg';
+import Instagram from '../draws/Instagram.svg';
+import Behance from '../draws/Behance.svg';
 
 export default function NavBar() {
+    const [open, setOpen] = useState(false);
+    const handleToggle = () => {
+        setOpen(!open);
+    }
     return(
         <header className="header">
             <div className="container">
-            <nav className="navbar">
-            <button className="hamburger hamburger--arrow" type="button">
+            <button  onClick={handleToggle} className={`hamburger hamburger--arrow ${open ? "is-active" : ""}`} type="button">
                 <span className="hamburger-box">
-                    <span className="hamburger-inner"></span>
+                    <span className={` hamburger-inner ${open ? "whiteLines" : ""}`} ></span>
                  </span>
             </button>
+            <nav className={`navbar ${open ? "active-nav" : ""}`}>
                     <ul>
                         <li>
                         <NavLink to="/" exact> Home </NavLink>
@@ -22,7 +31,14 @@ export default function NavBar() {
                         <li> <a href="">Portfolio</a></li>
                         <li><a href=""> About me</a></li>
                         <li><a href=""> Contact </a></li>
-                    </ul>
+                        <div className="icons">
+                            <a href=""> <img src={LinkeDin} alt=""/></a>
+                            <a href=""> <img src={Instagram} alt=""/></a>
+                            <a href=""> <img src={CodePen} alt=""/></a>
+                            <a href=""> <img src={GitHub} alt=""/></a>
+                            <a href=""> <img src={Behance} alt=""/></a>
+                        </div>
+                    </ul> 
                 </nav>
             </div>
         </header>
