2399a7d9062985621a8ba159f9a62821b834a56b
dehorus <ruizm.aj@gmail.com>
2021-05-18 12:53:53 -0600

    helper deleted

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom';
-import { debounce } from  '../helpers/Helper';
+import { debounce } from  '../Helper';
 import "./NavBar.scss";
 import LinkeDin from '../draws/LinkeDin.svg';
 import CodePen from '../draws/CodePen.svg';
