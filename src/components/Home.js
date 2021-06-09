fd2defd8e581f64eca4537e6977d1d0227a5b916
dehorus <ruizm.aj@gmail.com>
2021-06-08 22:27:05 -0600

    Parallax of Home.js deleted

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,160 +1,153 @@
-import React from 'react'
+import React from 'react';
 import { useSpring, animated, config } from 'react-spring';
-import {Parallax, ParallaxLayer,} from '@react-spring/parallax';
-import logo from '../logo.svg'
-import "./Home.scss";
+import logo from '../logo.svg';
+import './Home.scss';
 import MoonIllustration from './MoonIllustration';
 import Particles from 'react-particles-js';
 import About from './About';
-import sparkles from '../draws/Sparkles.svg'
-import sparkles1 from '../draws/Sparkles-1.svg'
+import sparkles from '../draws/Sparkles.svg';
+import sparkles1 from '../draws/Sparkles-1.svg';
 import Footer from './Footer';
 import { NavLink } from 'react-router-dom';
-import html5 from '../draws/skills/HTML5.svg'
-import css3 from '../draws/skills/CSS.svg'
-import javascriptlogo from '../draws/skills/JS.svg'
-import sass from '../draws/skills/SASS.svg'
-import reactjs from '../draws/skills/REACT.svg'
-import rocket from "../draws/rocket-vector.svg"
+import html5 from '../draws/skills/HTML5.svg';
+import css3 from '../draws/skills/CSS.svg';
+import javascriptlogo from '../draws/skills/JS.svg';
+import sass from '../draws/skills/SASS.svg';
+import reactjs from '../draws/skills/REACT.svg';
+import rocket from '../draws/rocket-vector.svg';
 
-export default  function Home() {
-   
-    const translateUP = useSpring({
-        from: {
-            transform: "translateY(2em)",
-            opacity: 0
-        },
+export default function Home() {
+	const translateUP = useSpring({
+		from: {
+			transform: 'translateY(2em)',
+			opacity: 0,
+		},
 
-        to: {
-            transform: "translateY(0)",
-            opacity: 1
-        },
-        delay: 1000,
-        config: config.wobbly,
-    });
+		to: {
+			transform: 'translateY(0)',
+			opacity: 1,
+		},
+		delay: 1000,
+		config: config.wobbly,
+	});
+	return (
+		<main className='home'>
+			<div className='hero'>
+				<div className='container-mobile'>
+					<div className='text-container'>
+						<animated.img className='mylogo' src={logo} alt='myLogo' />
+						<animated.div className='text' style={translateUP}>
+							<h3>front-end developer</h3>
+						</animated.div>
+					</div>
+				</div>
+				<Particles
+					id='particles-js'
+					className='particles-star'
+					params={{
+						particles: {
+							number: {
+								value: 250,
+								density: {
+									enable: true,
+									value_area: 1000,
+								},
+							},
+							color: {
+								value: ['#BD10E0', '#B8E986', '#50E3C2', '#FFD300', '#E86363'],
+							},
+							opacity: {
+								value: 1,
+								anim: {
+									enable: true,
+								},
+							},
+							size: {
+								value: 4,
+								random: true,
+								anim: {
+									enable: true,
+									speed: 3,
+								},
+							},
+							line_linked: {
+								enable: false,
+							},
+							move: {
+								speed: 0.2,
+							},
+						},
+					}}
+				></Particles>
+				<MoonIllustration></MoonIllustration>
+			</div>
 
-    return (
-        <main className="home">
-            <Parallax config={config.molasses} pages={2.77} >
-                <ParallaxLayer  offset= {0} speed={2.5}>
-                    <div className="hero">
-                        <div className="container-mobile">
-                            <ParallaxLayer offset={0.1} speed={1.5}>
-                            <div className="text-container">
-                                <animated.img className="mylogo" src={logo} alt="myLogo"/>
-                                <animated.div className="text" style={translateUP}>
-                                    <h3>front-end developer</h3>
-                                </animated.div>
-                            </div>
-                            </ParallaxLayer>
-                        </div>
-                    <Particles id="particles-js" className="particles-star"
-                        params={ { 
-                            particles: {
-                                number: {
-                                    value: 250,
-                                    density: {
-                                        enable: true,
-                                        value_area: 1000
-                                    }
-                                },
-                                color: {
-                                    value: ["#BD10E0","#B8E986","#50E3C2","#FFD300","#E86363"]
-                                },
-                                opacity: {
-                                    value: 1,
-                                    anim: {
-                                        enable: true
-                                    }
-                                },
-                                size: {
-                                    value: 4,
-                                    random: true,
-                                    anim: {
-                                        enable: true,
-                                        speed: 3
-                                    }
-                                },
-                                line_linked: {
-                                    enable: false
-                                },
-                                move: {
-                                    speed: 0.2
-                                }
-                                }
-                            }
-                        }
-                    ></Particles>
-                    <MoonIllustration></MoonIllustration>
-                    </div>
-                </ParallaxLayer>
-                <ParallaxLayer factor={1} offset={0.999} speed={2}> 
-                    <img className="sparkle-1" src={sparkles} alt="sparkles"/>
-                    <img className="sparkle-2" src={sparkles1} alt="sparkles"/>
-                </ParallaxLayer>
-                <ParallaxLayer factor={1.5}  offset={0.999} speed = {1.5}>
-                    <About></About>
-                </ParallaxLayer>
-                <ParallaxLayer factor={1} offset={1} speed={2}> 
-                    <img className="sparkle-3" src={sparkles} alt="sparkles"/>
-                    <img className="sparkle-4" src={sparkles1} alt="sparkles"/>
-                </ParallaxLayer>
-                <ParallaxLayer factor={1} offset={1.7} speed={2.5}>
-                    <div className="skills-section">
-                        <h2> Skills & knolewedge </h2>
-                        <div className="skills">
-                            <div className="tecn htmlbox">
-                                 <h3> HTML5 </h3>
-                                 <img src={html5} alt="html5" />
-                            </div>
-                            <div className="tecn cssbox">
-                                 <h3> CSS3 </h3>
-                                 <img src={css3} alt="html5" />
-                            </div>
-                            <div className="tecn javasbox">
-                                 <h3> JavaScript </h3>
-                                 <img src={javascriptlogo} alt="html5" />
-                            </div>
-                            <div className="tecn sassbox">
-                                 <h3> SASS </h3>
-                                 <img src={sass} alt="html5" />
-                            </div>
-                            <div className="tecn reactbox">
-                                 <h3> ReactJS </h3>
-                                 <img src={reactjs} alt="html5" />
-                            </div>
-                        </div>
-                        <div className ="other-technologys">
-                            <h3> Other technologys 👨‍💻</h3>
-                            <div className="bases-tec">
-                                <div className="develop">
-                                <h4> Development 🧑‍🚀 </h4>
-                                    <ul>
-                                        <li> Webpack </li>
-                                        <li> WordPress </li>
-                                        <li> mySQL </li>
-                                    </ul>   
-                                </div>
-                                <div className="design-sec">
-                                    <h4> Design 🎨 </h4>
-                                        <ul>
-                                            <li> Figma </li>
-                                            <li> Adobe XD</li>
-                                            <li> Adobe Illustrator </li>
-                                        </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="portfolio-contact">
-                            <NavLink to="/portfolio"> Portfolio <span> 🚀 </span> </NavLink>
-                            <NavLink to="/contact"> Contact <span> 📝 </span> </NavLink>
-                            <img src={rocket} alt="rocket" />
-                        </div>
-                        <Footer></Footer>
-                    </div>
+			<img className='sparkle-1' src={sparkles} alt='sparkles' />
+			<img className='sparkle-2' src={sparkles1} alt='sparkles' />
 
-                </ParallaxLayer>
-            </Parallax>   
-        </main>
-    ) 
+			<About></About>
+
+			<img className='sparkle-3' src={sparkles} alt='sparkles' />
+			<img className='sparkle-4' src={sparkles1} alt='sparkles' />
+
+			<div className='skills-section'>
+				<h2> Skills & knolewedge </h2>
+				<div className='skills'>
+					<div className='tecn htmlbox'>
+						<h3> HTML5 </h3>
+						<img src={html5} alt='html5' />
+					</div>
+					<div className='tecn cssbox'>
+						<h3> CSS3 </h3>
+						<img src={css3} alt='html5' />
+					</div>
+					<div className='tecn javasbox'>
+						<h3> JavaScript </h3>
+						<img src={javascriptlogo} alt='html5' />
+					</div>
+					<div className='tecn sassbox'>
+						<h3> SASS </h3>
+						<img src={sass} alt='html5' />
+					</div>
+					<div className='tecn reactbox'>
+						<h3> ReactJS </h3>
+						<img src={reactjs} alt='html5' />
+					</div>
+				</div>
+				<div className='other-technologys'>
+					<h3> Other technologys 👨‍💻</h3>
+					<div className='bases-tec'>
+						<div className='develop'>
+							<h4> Development 🧑‍🚀 </h4>
+							<ul>
+								<li> Webpack </li>
+								<li> WordPress </li>
+								<li> mySQL </li>
+							</ul>
+						</div>
+						<div className='design-sec'>
+							<h4> Design 🎨 </h4>
+							<ul>
+								<li> Figma </li>
+								<li> Adobe XD</li>
+								<li> Adobe Illustrator </li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div className='portfolio-contact'>
+					<NavLink to='/portfolio'>
+						{' '}
+						Portfolio <span> 🚀 </span>{' '}
+					</NavLink>
+					<NavLink to='/contact'>
+						{' '}
+						Contact <span> 📝 </span>{' '}
+					</NavLink>
+					<img src={rocket} alt='rocket' />
+				</div>
+				<Footer></Footer>
+			</div>
+		</main>
+	);
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,78 +1,141 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
-import { debounce } from  '../Helper';
-import "./NavBar.scss";
+import { debounce } from '../Helper';
+import './NavBar.scss';
 import LinkeDin from '../draws/LinkeDin.svg';
 import CodePen from '../draws/CodePen.svg';
 import GitHub from '../draws/Github.svg';
 import Instagram from '../draws/Instagram.svg';
 import Behance from '../draws/Behance.svg';
-import BehanceBlack from '../draws/Behance-black.svg'
-import GithubBlack from '../draws/Github-black.svg'
-import LinkedinBlack from '../draws/LinkeDin-black.svg'
+import BehanceBlack from '../draws/Behance-black.svg';
+import GithubBlack from '../draws/Github-black.svg';
+import LinkedinBlack from '../draws/LinkeDin-black.svg';
 
 export default function NavBar() {
-    const [open, setOpen] = useState(false);
-    const handleToggle = () => {
-        setOpen(!open);
-    }
+	const [open, setOpen] = useState(false);
+	const handleToggle = () => {
+		setOpen(!open);
+	};
 
-    const [prevScrollPos, setPrevScrollPos] = useState(0);   
-    const [visible, setVisible] = useState(true);
+	const [prevScrollPos, setPrevScrollPos] = useState(0);
+	const [visible, setVisible] = useState(true);
 
-    const handleScroll = debounce(() => {     
-        const currentScrollPos = window.pageYOffset;
-    
-        setVisible((prevScrollPos > currentScrollPos && prevScrollPos - currentScrollPos > 70) || currentScrollPos < 10);
-    
-        setPrevScrollPos(currentScrollPos);
-      }, 100);    
+	const handleScroll = debounce(() => {
+		const currentScrollPos = window.pageYOffset;
 
-      useEffect(() => {     
-        window.addEventListener('scroll', handleScroll);
-        
-        return () => window.removeEventListener('scroll', handleScroll);
-    
-      }, [prevScrollPos, visible, handleScroll]);
+		setVisible(
+			(prevScrollPos > currentScrollPos && prevScrollPos - currentScrollPos > 70) ||
+				currentScrollPos < 10
+		);
 
-    return(
-        <header className="header" style={{ top: visible ? '0' : '-20em' }} >
-            <div className="container">
-            <button  onClick={handleToggle} className={`hamburger hamburger--arrow ${open ? "is-active" : ""}`} type="button">
-                <span className="hamburger-box">
-                    <span className={` hamburger-inner ${open ? "whiteLines" : ""}`} ></span>
-                 </span>
-            </button>
-            <nav className={`navbar ${open ? "active-nav" : ""}`}>
-                    <ul>
-                        <li>
-                        <NavLink to="/" exact> Home </NavLink>
-                        </li>
-                        <li>
-                        <NavLink to="/post"> Blog </NavLink>
-                        </li>
-                        <li> 
-                        <NavLink to="/portfolio"> Portfolio </NavLink>    
-                        </li>
-                        <li>
-                        <NavLink to="/contact"> Contact </NavLink>
-                        </li>
-                        <h3 className="title-social">My social media. </h3>
-                        <div className="icons">
-                            <a href="https://www.linkedin.com/in/angelruiz95/" target="_blank" rel="noreferrer"> <img src={LinkeDin} alt="" /></a>
-                            <a href="https://www.instagram.com/angelus_dev/" target="_blank" rel="noreferrer"> <img src={Instagram} alt="" /></a>
-                            <a href="https://codepen.io/_josueangel" target="_blank" rel="noreferrer"> <img src={CodePen} alt="" /></a>
-                            <a href="https://github.com/dehorus" target="_blank" rel="noreferrer"> <img src={GitHub} alt="" /></a>
-                            <a href="https://www.behance.net/angelruiz12" target="_blank" rel="noreferrer"> <img src={Behance} alt="" /></a>
-                        </div>
-                    </ul>
-                    <div className="icons-desktop">
-                        <a href="https://www.linkedin.com/in/angelruiz95/" target="_blank" rel="noreferrer"> <img src={LinkedinBlack} alt="linkedin" /></a>
-                        <a href="https://github.com/dehorus" target="_blank" rel="noreferrer"> <img src={GithubBlack} alt="github" target="_blank"/></a>
-                        <a href="https://www.behance.net/angelruiz12" target="_blank" rel="noreferrer"> <img src={BehanceBlack} alt="behance"target="_blank" /></a>
-                    </div> 
-                </nav>
-            </div>
-        </header>
-    )
+		setPrevScrollPos(currentScrollPos);
+	}, 100);
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
+
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, [prevScrollPos, visible, handleScroll]);
+
+	return (
+		<header
+			className='header'
+			style={{
+				top: visible ? '0' : '-20em',
+				background: visible ? '#fed9b700' : '#fed9b7',
+			}}
+		>
+			<div className='container'>
+				<button
+					onClick={handleToggle}
+					className={`hamburger hamburger--arrow ${open ? 'is-active' : ''}`}
+					type='button'
+				>
+					<span className='hamburger-box'>
+						<span className={` hamburger-inner ${open ? 'whiteLines' : ''}`}></span>
+					</span>
+				</button>
+				<nav className={`navbar ${open ? 'active-nav' : ''}`}>
+					<ul>
+						<li>
+							<NavLink to='/' exact>
+								{' '}
+								Home{' '}
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/post'> Blog </NavLink>
+						</li>
+						<li>
+							<NavLink to='/portfolio'> Portfolio </NavLink>
+						</li>
+						<li>
+							<NavLink to='/contact'> Contact </NavLink>
+						</li>
+						<h3 className='title-social'>My social media. </h3>
+						<div className='icons'>
+							<a
+								href='https://www.linkedin.com/in/angelruiz95/'
+								target='_blank'
+								rel='noreferrer'
+							>
+								{' '}
+								<img src={LinkeDin} alt='' />
+							</a>
+							<a
+								href='https://www.instagram.com/angelus_dev/'
+								target='_blank'
+								rel='noreferrer'
+							>
+								{' '}
+								<img src={Instagram} alt='' />
+							</a>
+							<a
+								href='https://codepen.io/_josueangel'
+								target='_blank'
+								rel='noreferrer'
+							>
+								{' '}
+								<img src={CodePen} alt='' />
+							</a>
+							<a href='https://github.com/dehorus' target='_blank' rel='noreferrer'>
+								{' '}
+								<img src={GitHub} alt='' />
+							</a>
+							<a
+								href='https://www.behance.net/angelruiz12'
+								target='_blank'
+								rel='noreferrer'
+							>
+								{' '}
+								<img src={Behance} alt='' />
+							</a>
+						</div>
+					</ul>
+					<div className='icons-desktop'>
+						<a
+							href='https://www.linkedin.com/in/angelruiz95/'
+							target='_blank'
+							rel='noreferrer'
+						>
+							{' '}
+							<img src={LinkedinBlack} alt='linkedin' />
+						</a>
+						<a href='https://github.com/dehorus' target='_blank' rel='noreferrer'>
+							{' '}
+							<img src={GithubBlack} alt='github' target='_blank' />
+						</a>
+						<a
+							href='https://www.behance.net/angelruiz12'
+							target='_blank'
+							rel='noreferrer'
+						>
+							{' '}
+							<img src={BehanceBlack} alt='behance' target='_blank' />
+						</a>
+					</div>
+				</nav>
+			</div>
+		</header>
+	);
 }
